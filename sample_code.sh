d91f9fc00552eeb7b5cf2ba7700fc8dc629af0fc
naveensoma8125 <114895571+naveensoma8125@users.noreply.github.com>
2023-08-23 20:47:56 +0530

    Update sample_code.sh

--- sample_code.sh
+++ sample_code.sh
@@ -3,6 +3,7 @@
 
 echo "This is an Samples Script from GitHub"
 echo "This is an example of a script"
+echo "hello world"
 echo "This is running in a build step and it shows changes"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
